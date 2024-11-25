3cca761a61e141d4f66fc21812fae2b6e04d2559
deucenn <panix.black@web.de>
2024-11-25 21:30:51 +0100

    Migrated to typescript

--- src/config/dotenvConfig.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    PRINTIFY_API_KEY: process.env.PRINTIFY_API_KEY,
-    SHOP_ID: process.env.SHOP_ID,
-    PORT: process.env.PORT || 3000,
-};
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const { PORT } = require("./config/dotenvConfig");
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
